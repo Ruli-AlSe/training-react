fefc645914b22444ec892ca8735e359240f8461f
Raul Alejandro Almanza Serrano <ra.almanzaserrano@gmail.com>
2020-03-25 13:41:01 -0600

    Try React state in a dummy function

--- src/components/TestsComponent.js
+++ src/components/TestsComponent.js
@@ -3,14 +3,50 @@ import MyComponent from "./MyComponent";
 import Movies from "./Movies";
 
 class TestsComponent extends React.Component {
-  render() {
+/*
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    };
+  }
+*/
+
+  state = {
+    counter: 0
+  };
+
+  add() {
+    //this.counter += 1;
+    //this.state.counter += 1;
+    this.setState({
+      counter: (this.state.counter + 1)
+    });
+  }
+
+  subtrac() {
+    this.setState({
+      counter: (this.state.counter - 1)
+    });
+  }
+
+  render(e) {
     return (
       <section id="content">
-        <h2 class="subheader">Latest articles</h2>
+        <h2 className="subheader">Latest articles</h2>
         <section className="components">
           <MyComponent />
           <Movies />
         </section>
+
+        <h2 className="subheader">React States</h2>
+        <p>
+          {'Counter: ' + this.state.counter }
+        </p>
+        <p>
+          <input type="button" value="add" onClick={ this.add.bind(this) } />
+          <input type="button" value="subtrac" onClick={ this.subtrac.bind(this) } />
+        </p>
       </section>
     );
   }
